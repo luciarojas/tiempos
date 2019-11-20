accf5a502c9b840d8fe274550957a88206404898
LuciaRojas <luciarojas525@gmail.com>
2019-11-19 23:57:41 -0600

    login

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  public sorteos: Observable<any[]>;
+  constructor(public afAuth: AngularFireAuth, db: AngularFirestore) { 
+    this.sorteos = db.collection('sorteos').valueChanges();
+  }
+
+  ngOnInit() {
+
+  }
+
+  login() {
+    this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+  }
+  logout() {
+    this.afAuth.auth.signOut();
+  }
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDxzQCD3LOCuaoDokHqe4BBWKvXm4Kd5ug',
+    authDomain: 'tiempos-a6db9.firebaseapp.com',
+    databaseURL: 'https://tiempos-a6db9.firebaseio.com',
+    projectId: 'tiempos-a6db9',
+    storageBucket: 'tiempos-a6db9.appspot.com',
+    messagingSenderId: '323140456500'
+  }
+};
+
+
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
